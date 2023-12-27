ba393a9c61f961b30230dae324a11a17019f555c
EdwinZarateP <emzp1994@gmail.com>
2023-12-26 22:34:11 -0500

    modificacion8

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
+import Calculadora from './componentes/calculadora.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
+    <Calculadora/>
   </React.StrictMode>,
 )
