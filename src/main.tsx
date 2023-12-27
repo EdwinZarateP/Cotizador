6e336d5abd0f7dd3686be6f9c65a23266d0b6e04
EdwinZarateP <emzp1994@gmail.com>
2023-12-26 22:47:08 -0500

    modificacion13

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+import Imagen from './componentes/titulo.tsx'
 import Calculadora from './componentes/calculadora.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="contenedorPrincipal">
-
+      <Imagen/> 
       <Calculadora/>
 
     </div>
