68295a06117e6c43718fdbc7034a9f154555b9c9
EdwinZarateP <emzp1994@gmail.com>
2023-12-26 22:50:19 -0500

    modificacion14

---
+++ src/componentes/header.tsx
@@ -0,0 +1,12 @@
+import '../estilos/header.css'
+
+const Header = () => {
+
+  return (
+    <header className='encabezado'>
+      <h1>Cotizador de paqueteo</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Imagen from './componentes/titulo.tsx'
 import Calculadora from './componentes/calculadora.tsx'
+import Header from './componentes/header.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="contenedorPrincipal">
+      <Header/>
       <Imagen/> 
       <Calculadora/>
 
